081670c7d620603e6f41fb5562b10291960555cf
Chad Schadewald <ces83@students.uwf.edu>
2020-11-06 21:46:47 -0600

    Canvas basics

--- src/Main.java
+++ src/Main.java
@@ -5,6 +5,7 @@ import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.*;
 import javafx.scene.control.Button;
+import javafx.scene.layout.FlowPane;
 import javafx.scene.paint.*;
 import javafx.scene.canvas.*;
 import javafx.scene.layout.GridPane;
@@ -17,34 +18,49 @@ public class Main extends Application {
     public void start(Stage primaryStage) throws Exception{
 //        Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
         GridPane gridPane = new GridPane();
+        FlowPane flowPane = new FlowPane();
         Scene mainScene = new Scene(gridPane);
 
-        gridPane.setHgap(10);
-        gridPane.setVgap(10);
+        gridPane.setHgap(5);
+        gridPane.setVgap(5);
         gridPane.setPadding(new Insets(0, 10, 0, 10));
 
         Button startRace = new Button("Start Race");
         Button resetRace = new Button("Reset Race");
         Button exitRace = new Button("Exit Race");
 
-        Text testHorse1 = new Text("Horse1");
-        gridPane.add(testHorse1, 1, 0);
+        Canvas horse1 = new Canvas(500, 75);
+        GraphicsContext horse1GraphicsContext = horse1.getGraphicsContext2D();
+        horse1GraphicsContext.fillRect(0, 0, 600,75 );
 
-        Text testHorse2 = new Text("Horse2");
-        gridPane.add(testHorse2, 1, 1);
+        Canvas horse2 = new Canvas(500, 75);
+        GraphicsContext horse2GraphicsContext = horse2.getGraphicsContext2D();
+        horse2GraphicsContext.fillRect(0, 0, 600,75 );
 
-        Text testHorse3 = new Text("Horse3");
-        gridPane.add(testHorse3, 1, 2);
+        Canvas horse3 = new Canvas(500, 75);
+        GraphicsContext horse3GraphicsContext = horse3.getGraphicsContext2D();
+        horse3GraphicsContext.fillRect(0, 0, 600,75 );
 
-        Text testHorse4 = new Text("Horse4");
-        gridPane.add(testHorse4, 1, 3);
+        Canvas horse4 = new Canvas(500, 75);
+        GraphicsContext horse4GraphicsContext = horse4.getGraphicsContext2D();
+        horse4GraphicsContext.fillRect(0, 0, 600,75 );
 
-        Text testHorse5 = new Text("Horse5");
-        gridPane.add(testHorse5, 1, 4);
+        Canvas horse5 = new Canvas(500, 75);
+        GraphicsContext horse5GraphicsContext = horse5.getGraphicsContext2D();
+        horse5GraphicsContext.fillRect(0, 0, 600,75 );
 
-        gridPane.add(startRace, 1,5);
-        gridPane.add(resetRace, 2, 5);
-        gridPane.add(exitRace, 3, 5);
+        flowPane.setVgap(8);
+        flowPane.setHgap(4);
+        flowPane.getChildren().add(startRace);
+        flowPane.getChildren().add(resetRace);
+        flowPane.getChildren().add(exitRace);
+
+        gridPane.add(horse1, 1, 0);
+        gridPane.add(horse2, 1, 1);
+        gridPane.add(horse3, 1, 2);
+        gridPane.add(horse4, 1, 3);
+        gridPane.add(horse5, 1, 4);
+        gridPane.add(flowPane, 1, 5);
 
         Group root = new Group();
         primaryStage.setTitle("Welcome to Horse Race!");
